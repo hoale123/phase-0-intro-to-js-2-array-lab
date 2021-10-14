e537f3b15b4eb8bec3eceff028a2fdc4b28c55bf
Hoa Le <Hoa_le9485@yahoo.com>
2021-10-14 01:37:33 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Write your solution here!
+
+const cats = ['Milo', 'Otis', 'Garfield'];
+console.log(cats.length);
+
+function destructivelyAppendCat(name) {
+    cats.push(name);
+}
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+}
+function destructivelyRemoveLastCat(name) {
+    cats.pop(name);
+}
+function destructivelyRemoveFirstCat(name) {
+    cats.shift(name);
+}
+function appendCat(name) {
+    let newArray = ([...cats, name]);
+    return newArray
+}
+function prependCat(name) {
+    let newArray = ([name, ...cats]);
+    return newArray;
+}
+const bac = ['Milo', 'Otis', 'Garfield'];
+function removeLastCat() {
+   let dar =  bac.slice(0, bac.length - 1);
+    return dar 
+}
+const abc = ['Milo', 'Otis', 'Garfield'];
+function removeFirstCat() {
+   let ddd =  abc.slice(1);
+    return ddd 
+}
